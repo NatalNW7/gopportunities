06188b2374fcb9799dbff74b6f389808241ad441
NatalNW7 <natanaelweslley1@gmail.com>
2024-08-06 22:29:56 -0300

    feat: add routes

--- router/routes.go
+++ router/routes.go
@@ -1,18 +1,25 @@
 package router
 
 import (
+	"github.com/NatalNW7/gopportunities/docs"
 	"github.com/NatalNW7/gopportunities/handler"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(server *gin.Engine){
 	handler.InitializeHandler()
-	v1 := server.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := server.Group(basePath)
 	{
-		// v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
-		// v1.GET("/openings", handler.ListOpeningsHandler)
-		// v1.PUT("/opening", handler.UpdateOpeningHandler)
-		// v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningsHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 	}
+
+	server.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
